fccbece234ba43b85ebb35903b7179f34fa56a17
mhager3 <mhager3@hotmail.com>
2018-03-12 22:31:14 -0400

    committing 20180312

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -33,35 +33,46 @@ voteCtr = {}
 highvote = 0
 winner = ""
 
+# open the file
 with open(csvpath1, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
 
+    # skip the first line (header)
     next(csvreader)
 
+    # go through each row, add total votes
     for row in csvreader:
         totVotes = totVotes + 1
 
+        # add to dictionary if it does not exist, increment value by 1
+        # if the key already exists in dictionary
+        # Help: https://stackoverflow.com/questions/1692388/python-list-of-dict-if-exists-increment-a-dict-value-if-not-append-a-new-dic
         if row[2] not in voteCtr.keys():
             voteCtr[row[2]] = 1
         else:
             voteCtr[row[2]] = voteCtr[row[2]] + 1
 
-
+# print to terminal
 print("\nElection Results")
 print("------------------------------")
 print("Total Votes: ", totVotes )
 print("------------------------------")
-#print(voteCtr)
+
+# loop through dictionary
 for keys in voteCtr:
     percentage = ((voteCtr[keys]/totVotes)*100)
     
-    #print("%s: %.1f " % (keys, percentage), voteCtr[keys])
+    # print each key (name), percentage of votes, and number of votes
     print(f'{keys}: {percentage:.1f}% ({voteCtr[keys]})')
+
+    # find the winner
     if voteCtr[keys] > highvote:
         highvote = voteCtr[keys]
         winner = keys
+
 print("------------------------------")
 print(f'Winner: {winner}')
 print("------------------------------")
 
+# print to file
 printToFile(totVotes, voteCtr)
